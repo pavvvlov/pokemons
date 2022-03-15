f63d64893e3c81622a98b53bdd44079e1a6b1dcf
Stanko Yanev <stankopk@abv.bg>
2022-03-15 10:50:01 +0200

    Add project files.

---
+++ pokemons/Controllers/pokemonsController.cs
@@ -0,0 +1,20 @@
+﻿using pokemons.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokemons.Controllers
+{
+    class pokemonsController
+    {
+        public List<pokemon> GetPokemons()
+        {
+            using (Database1Entities ex = new Database1Entities())
+            {
+                return ex.pokemons.ToList();
+            }
+        }
+    }
+}
---
+++ pokemons/View/pokemonsView.cs
@@ -0,0 +1,27 @@
+﻿using pokemons.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pokemons
+{
+    public partial class pokemonsView : Form
+    {
+        pokemonsController p = new pokemonsController();
+        public pokemonsView()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvPokemons.DataSource = p.GetPokemons();
+        }
+    }
+}
