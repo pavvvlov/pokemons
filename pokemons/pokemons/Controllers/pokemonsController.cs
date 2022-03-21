dbd2cb54ec67270c1daff57b4153581f373bde7f
Nonbaka <86018544+Nonbaka@users.noreply.github.com>
2022-03-21 20:43:11 +0200

    Add files via upload

---
+++ pokemons/pokemons/Controllers/pokemonsController.cs
@@ -0,0 +1,30 @@
+﻿using pokemons.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokemons.Controllers
+{
+    class pokemonsController
+    {
+        public List<pokemon> GetPokemons()
+        {
+            using (Database1Entities ex = new Database1Entities())
+            {
+                return ex.pokemons.ToList();
+            }
+            {
+                var pokemonsToUpdate = ex.pokemons.Where(p => p.Id == id).FirstOrDefault();
+                if (pokemonsToUpdate != null)
+                {
+                    pokemonsToUpdate.Id = s.id;
+                    pokemonsToUpdate.Name = s.Name;
+                    pokemonsToUpdate.Age = s.Age;
+                    ex.SaveChanges();
+                }
+            }
+        }
+    }
+}
